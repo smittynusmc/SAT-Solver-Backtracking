51202c5f38a77f018910e5c694da297fd8e4135e
Dennis <Dennis@150.250.47.222>
2016-03-22 11:19:58 -0400

    updated Formula Class
    modified readFromFile method slightly
    Added some new variables 
    other stuff i can't think of

--- SAT Solver Backtracking/src/sat_solver_backtracking/Formula.java
+++ SAT Solver Backtracking/src/sat_solver_backtracking/Formula.java
@@ -10,18 +10,92 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Formula {
-	
-    
-    // list of clauses in CNF 
-    private List<Clause> clauses;
-    
-    // The current partial or completed state for this formula
-    private List <Literal> states;
-    
-    //index for backtracking
-    int lastIndex;
-    
-    public int getLastIndex() {
+
+
+	// list of clauses in CNF 
+	private List<Clause> formulaList;
+
+	// The current partial or completed state for this formula
+	private List <Literal> successState;
+
+	//index for backtracking
+	int lastIndex;
+
+	// number of variables in the formula
+	int numVariables;
+
+	// number of clauses in the formula
+	int numClauses;
+
+	//True if any clause is empty
+	boolean hasEmptyClause = false;
+
+	//keeps track of the smallest sized clause in the list 
+	private int minClauseSize;
+
+	//keeps track of the index the first smallest clause
+	private int minClauseIndex;
+
+
+	/**
+	 * This constructor takes no arguments
+	 */
+	public Formula()
+	{
+		formulaList = new ArrayList<Clause>(500);
+		successState = new ArrayList <Literal> (500);
+		numVariables = 0;
+		minClauseSize=Integer.MAX_VALUE;
+		minClauseIndex=-1;
+	}
+
+
+
+
+	/**
+	 * This method returns the number of variables in the formula
+	 * @return the number of variables
+	 */
+	public int getNumVariables() 
+	{
+		return numVariables;
+		//use for keeping track of index?
+	}
+
+
+	/**
+	 * This method sets the number of variables in the formula
+	 * @param numVariables number of variables
+	 * @return void
+	 */
+	public void setNumVariables(int numVariables) 
+	{
+		this.numVariables = numVariables;
+	}
+
+	/**
+	 * This method returns the number of clauses in the formula
+	 * @return the number of clauses
+	 */
+	public int getNumClauses() 
+	{
+		return numClauses;
+	}
+
+
+	/**
+	 * This method sets the number of clauses in the formula
+	 * @param numClauses number of clauses
+	 * @return void
+	 */
+	//public void setNumClauses(int numClauses) 
+	//{
+	//	this.numClauses = numClauses;
+	//}
+
+
+
+	public int getLastIndex() {
 		return lastIndex;
 	}
 
@@ -29,132 +103,85 @@ public class Formula {
 		this.lastIndex = lastIndex;
 	}
 
-	// number of variables in the formula
-    int numVariables;
-    
-    // number of clauses in the formula
-    int numClauses;
-    
-    //True if any clause is empty
-    boolean hasEmptyClause = false;
-    
-    
-    /**
-     * This constructor takes no arguments
-     */
-    public Formula()
-    {
-        clauses = new ArrayList<Clause>();
-        states = new ArrayList <Literal> ();
-        numVariables = 0;
-    }
 
-    /**
-     * This method returns the number of variables in the formula
-     * @return the number of variables
-     */
-    public int getNumVariables() 
-    {
-        return numVariables;
-    }
 
-    
-    /**
-     * This method sets the number of variables in the formula
-     * @param numVariables number of variables
-     * @return void
-     */
-    public void setNumVariables(int numVariables) 
-    {
-        this.numVariables = numVariables;
-    }
+	/**
+	 * This method adds a clause to this formula
+	 * @param c a Clause
+	 * @return void
+	 */
+	public void addClause(Clause c)
+	{
+		formulaList.add(c);
+		if(c.size()<minClauseSize)
+		{
+			minClauseSize = c.size();
+			minClauseIndex = formulaList.size()+1;
+			if(minClauseSize==0)
+			{
+				hasEmptyClause=true;
+			}
+		}
 
-    /**
-     * This method returns the number of clauses in the formula
-     * @return the number of clauses
-     */
-    public int getNumClauses() 
-    {
-        return numClauses;
-    }
+	}
 
-    
-    /**
-     * This method sets the number of clauses in the formula
-     * @param numClauses number of clauses
-     * @return void
-     */
-    public void setNumClauses(int numClauses) 
-    {
-        this.numClauses = numClauses;
-    }
-    
-    
-    /**
-     * This method loads and returns a formula from a file specification
-     * @param filename name of the file containing the formula
-     * @return the Formula
-     */
-    public static Formula readFromFile(String filename)
-    {
-        BufferedReader br = null;
-        String line = null;
-        int numVars, numClauses, var;
-        Formula f = new Formula();
-        try {
-            br = new BufferedReader(new FileReader(filename));
-            while((line = br.readLine()) != null){          
-                if(line.startsWith("c"))
-                    continue;
-                if(line.length() == 0)
-                    continue;
-                if(line.startsWith("p cnf")){
-                    line = line.replaceAll("\\s+", " ");
-                    String[] data = line.split("[ \t]");
-                    numVars = Integer.parseInt(data[2]);
-                    numClauses = Integer.parseInt(data[3]);
-                    f.setNumVariables(numVars);
-                    f.setNumClauses(numClauses);
-                }
-                else{
-                    Scanner sc = new Scanner(line);
-                    Clause c = new Clause();
-                    while(sc.hasNextInt()){
-                        var = sc.nextInt();
-                        if(var == 0)
-                            break;
-                        else if(var > 0){
-                            c.add(new Literal(var));
-                        }  
-                        else{
-                            c.add(new Literal(var));
-                        }
-                    }
-                    f.addClause(c);
-                    sc.close();
-                }
-            }
-            br.close();
-        } catch (FileNotFoundException ex) {
-            System.out.println("file I/O error: " + ex);
-        } catch (IOException ex) {
-            System.out.println("file I/O error: " + ex);
-        }
-        return f;
-    }
-    
-    /**
-     * This method adds a clause to this formula
-     * @param c a Clause
-     * @return void
-     */
-    public void addClause(Clause c)
-    {
-        clauses.add(c);
-    }
-    
-    public boolean isEmpty () {
-    	return clauses.isEmpty();
-    }
-   
+	public boolean isEmpty () {
+		return formulaList.isEmpty();
+	}
+
+
+	/**
+	 * This method loads and returns a formula from a file specification
+	 * @param filename name of the file containing the formula
+	 * @return the Formula
+	 */
+	public static Formula readFromFile(String filename)
+	{
+		BufferedReader br = null;
+		String line = null;
+		int numVars, numClauses, var;
+		Formula f = new Formula();
+		try {
+			br = new BufferedReader(new FileReader(filename));
+			while((line = br.readLine()) != null){          
+				if(line.startsWith("c"))
+					continue;
+				if(line.length() == 0)
+					continue;
+				if(line.startsWith("p cnf")){
+					line = line.replaceAll("\\s+", " ");
+					String[] data = line.split("[ \t]");
+					numVars = Integer.parseInt(data[2]);
+					numClauses = Integer.parseInt(data[3]);
+					f.setNumVariables(numVars);
+					//f.setNumClauses(numClauses);
+				}
+				else{
+					Scanner sc = new Scanner(line);
+					Clause c = new Clause();
+					while(sc.hasNextInt()){
+						var = sc.nextInt();
+						if(var == 0)
+							break;
+						else if(var > 0){
+							c.add(new Literal(var));
+						}  
+						else{
+							c.add(new Literal(var));
+						}
+					}
+					f.addClause(c);
+					sc.close();
+				}
+			}
+			br.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("file I/O error: " + ex);
+		} catch (IOException ex) {
+			System.out.println("file I/O error: " + ex);
+		}
+		return f;
+	}
+
+
 }
